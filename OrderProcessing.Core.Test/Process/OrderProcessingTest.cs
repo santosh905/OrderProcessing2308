588379d76a68bb02748a9446a244843aee653823
Sajjan <Santosh.Sajjan@philips.com>
2021-06-04 15:51:24 +0530

    Add project files.

---
+++ OrderProcessing.Core.Test/Process/OrderProcessingTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessing.Core.Process
+{
+    [TestClass]
+    public class OrderProcessingTest
+    {
+        [TestMethod]
+        public void If_the_payment_is_for_a_physical_product_generate_a_packing_slip_for_shipping()
+        {
+
+        }
+        [TestMethod]
+        public void If_the_payment_is_for_a_book_create_duplicate_packing_slip_for_the_royalty_department()
+        {
+
+        }
+
+        [TestMethod]
+        public void If_the_payment_is_for_a_membership_activate_that_membership()
+        {
+
+        }
+
+        [TestMethod]
+        public void If_the_payment_is_an_upgrade_to_a_membership_apply_theupgrade()
+        {
+
+        }
+
+    }
+}
