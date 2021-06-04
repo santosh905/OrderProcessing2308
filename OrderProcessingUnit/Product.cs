2383a9e4e93b81d0c771f89e0d04eb405726b287
santosh_sajjan <santosh2308@gmail.com>
2021-06-04 16:08:24 +0530

    Initial design of testcase on orderProcessingUnit

---
+++ OrderProcessingUnit/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessingUnit
+{
+    public class Product
+    {
+        private string _shippingAddress;
+        private string _name;
+        private readonly string _productCode;
+        private ProductType _productType;
+
+        public Product(string name)
+        {
+            this._name = name;
+            _productCode = Guid.NewGuid().ToString("N");
+        }
+
+        public void SetShippingAddress(string shippingAddress)
+        {
+            this._shippingAddress = shippingAddress;
+        }
+
+        public string GetShippingAddress()
+        {
+            return _shippingAddress;
+        }
+
+        public string GetProductCode()
+        {
+            return _productCode;
+        }
+
+        public void SetProductType(ProductType productType)
+        {
+            this._productType = productType;
+        }
+
+        public ProductType GetProductType()
+        {
+            return this._productType;
+        }
+    }
+}
---
+++ OrderProcessingUnit/ProductType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessingUnit
+{
+    public enum ProductType
+    {
+        physical_product,
+        book,
+        membership,
+        membershipUpgrade,
+        video
+    }
+}
